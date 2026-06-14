d8944f6f82698fa871dc32661d174e500325e8ca
BranellKaunda <branellk@gmail.com>
2026-06-14 17:51:53 +0200

    fix: teams-x-leagues endpoint

--- server/api/teams-x-leagues/index.ts
+++ server/api/teams-x-leagues/index.ts
@@ -1,44 +1,26 @@
 import { defineEventHandler, getQuery } from "h3";
 import { useDrizzle } from "#server/utils/drizzle";
-import { teamsXleagues, teams, leagues } from "#server/database/schema";
-import { eq, and } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
 
-  const conditions = [];
-  if (query.teamId) {
-    conditions.push(eq(teamsXleagues.teamId, Number(query.teamId)));
-  }
-  if (query.leagueId) {
-    conditions.push(eq(teamsXleagues.leagueId, Number(query.leagueId)));
-  }
+  const OR = [
+    query.teamId && { teamId: Number(query.teamId) },
+    query.leagueId && { leagueId: Number(query.leagueId) },
+  ].filter(Boolean);
 
   const db = useDrizzle();
 
-  const result = await db
-    .select({
-      id: teamsXleagues.id,
-      teamId: teamsXleagues.teamId,
-      leagueId: teamsXleagues.leagueId,
-      team: {
-        id: teams.id,
-        name: teams.name,
-        logo: teams.logo,
-        location: teams.location,
-      },
-      league: {
-        id: leagues.id,
-        name: leagues.name,
-        season: leagues.season,
-        logo: leagues.logo,
-        rank: leagues.rank,
-      },
-    })
-    .from(teamsXleagues)
-    .innerJoin(teams, eq(teamsXleagues.teamId, teams.id))
-    .innerJoin(leagues, eq(teamsXleagues.leagueId, leagues.id))
-    .where(conditions.length > 0 ? and(...conditions) : undefined);
+  const result = await db.query.teamsXleagues?.findMany({
+    columns: {
+      id: true,
+    },
+    with: {
+      team: true,
+      league: true,
+    },
+    where: { OR: OR as any },
+  });
 
   return result;
 });
